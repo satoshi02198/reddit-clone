9715f0c0b881652cf4958c419289119cea6ab9a7
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-04-23 17:02:29 +0900

    202304231702

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -25,7 +25,7 @@ type NewPostFormProps = {
   communityImageURL?: string;
 };
 
-const formTabs: TabItem[] = [
+const formTabs: TabItems[] = [
   {
     title: "Post",
     icon: IoDocumentText,
@@ -48,7 +48,7 @@ const formTabs: TabItem[] = [
   },
 ];
 
-export type TabItem = {
+export type TabItems = {
   title: string;
   icon: typeof Icon.arguments;
 };
