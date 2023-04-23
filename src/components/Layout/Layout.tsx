9ddffe139a8504ab9730c9d0b475b1228de696a1
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-04-23 19:03:21 +0900

    20230423

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Navbar from "../Navbar/Navbar";
 
-const Layout: React.FC = ({ children }: any) => {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Navbar />
